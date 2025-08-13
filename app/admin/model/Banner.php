355763a53dce9f49f4b8eea11519365011d4f568
Cleaner <youziyouzishu@gmail.com>
2025-08-13 16:40:41 +0800

    feat(admin): 添加轮播图功能
    - 新增 Banner 模型和相关控制器
    - 实现轮播图的浏览、新增、编辑和删除功能
    - 优化订单和订阅页面的渲染逻辑

--- app/admin/model/Banner.php
+++ app/admin/model/Banner.php
@@ -6,15 +6,10 @@ use plugin\admin\app\model\Base;
 
 /**
  * @property integer $id 主键(主键)
- * @property string $image 封面
- * @property string $content 内容
+ * @property string $image 图片
  * @property integer $weight 权重
  * @property string $created_at 创建时间
  * @property string $updated_at 更新时间
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Banner newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Banner newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Banner query()
- * @mixin \Eloquent
  */
 class Banner extends Base
 {
